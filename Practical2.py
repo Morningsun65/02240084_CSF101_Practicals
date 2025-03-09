ca8dc0be57add3628a38df5ef076335d9ad9a038
Minjur65 <02240084.cst@rub.edu.bt>
2025-03-09 20:59:54 +0600

    my second commit to Practical2.py

--- Practical2.py
+++ Practical2.py
@@ -1,5 +1,5 @@
 #Array
-'''first_array = [7, 2.3, True, "Sonam", None]
+first_array = [7, 2.3, True, "Sonam", None]
 first_array.append(3)
 #append will add in the element in the array 
 # python already knows it has 5 element si it will put 3 in the array and not create another copy
@@ -8,8 +8,7 @@ firstArrayLength = len(first_array)
 
 second_Array =[7, 2.3, True, "Sonam", None]
 secondArrayLength = len(second_Array)
-
-print(firstArrayLength - secondArrayLength)'''
+print(firstArrayLength - secondArrayLength)
 
 
 
@@ -25,18 +24,18 @@ print(firstArrayLength - secondArrayLength)'''
 
 # LOOP
 
-'''ProgramList = ["Civil","EE","ECE","MECH","IT","Arch","SWE" ,"WRE","EG","ICE"]
+ProgramList = ["Civil","EE","ECE","MECH","IT","Arch","SWE" ,"WRE","EG","ICE"]
 arrayLength = len(ProgramList)
 
 for i in range(arrayLength):
-    print(ProgramList[i])'''
+    print(ProgramList[i])
     
 
 # while loop
 
-'''ProgramList = ["Civil","EE","ECE","MECH","IT","Arch","SWE" ,"WRE","EG","ICE"]
+ProgramList = ["Civil","EE","ECE","MECH","IT","Arch","SWE" ,"WRE","EG","ICE"]
 arraylen = len(ProgramList)
 index = 0
 while index < arraylen:
     print(ProgramList[index])
-    index = index + 1 '''
+    index = index + 1 
