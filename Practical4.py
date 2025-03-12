4f5532a0c0222fd81b106d0c257eb4833d27717b
Minjur65 <02240084.cst@rub.edu.bt>
2025-03-12 15:35:08 +0600

    My first commit

---
+++ Practical4.py
@@ -0,0 +1,11 @@
+first_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+inverse_list = []
+
+index = len(first_list) - 1  
+
+
+while index >= 0:
+    inverse_list.append(first_list.pop())  
+    index -= 1  
+
+print(inverse_list)  
